b74b07c219ed73d8e31559b5c194370a0be2841d
Yogesh Kumar Singh <124029300+Yogesh-Kumar-Singh@users.noreply.github.com>
2024-10-14 15:39:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("incrementbtn").addEventListener("click",incrementCounter);
+document.getElementById("decrementbtn").addEventListener("click",decrementCounter);
+document.getElementById("resetbtn").addEventListener("click",resetCounter);
+
+let counterDisplay = document.getElementById("counterDisplay");
+let counterValue = 0;
+function updateCounterDisplay() {
+    counterDisplay.textContent = counterValue;
+}
+function incrementCounter() {
+    counterValue++;
+    updateCounterDisplay();
+}
+function decrementCounter() {
+    if (counterValue>0) {
+        counterValue--;
+        updateCounterDisplay();
+    }
+}
+function resetCounter() {
+    counterValue=0;
+    updateCounterDisplay();
+}
